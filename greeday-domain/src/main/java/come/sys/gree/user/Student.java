97a517e516c560b0a534b17d84cdbb002a71a0f6
YANG_sty <13849187852@163.com>
2020-04-05 14:48:46 +0800

    delete .class .idea

---
+++ greeday-domain/src/main/java/come/sys/gree/user/Student.java
@@ -0,0 +1,22 @@
+package come.sys.gree.user;
+
+/**
+ * Create by yang_zzu on 2020/4/5 on 14:43
+ */
+
+public class Student {
+    private String id;
+
+    private String name;
+
+    private String age;
+
+    private String idCar;
+
+    private String phone;
+
+    private String address;
+
+    private String email;
+
+}
